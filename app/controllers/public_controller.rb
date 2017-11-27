711d9819b4e5cb16de5af23c734fe7390a7e499a
katiefrost95 <31935825+katiefrost95@users.noreply.github.com>
2017-11-27 15:26:23 +0000

    Revert "Narullah links"

--- app/controllers/public_controller.rb
+++
@@ -1,6 +0,0 @@
-class PublicController < ApplicationController
-
-  def index
-    @instanceVariable = "Welcome to the Homepage"
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-r1 = Room.create(name: 'Room1', color: 'Blue')
-r2 = Room.create(name: 'Room2', color: 'Green')
-r3 = Room.create(name: 'Room3', color: 'red')
-r4 = Room.create(name: 'Room4', color: 'orange')
-r5 = Room.create(name: 'Room5', color: 'Purple')
-r6 = Room.create(name: 'Room6', color: 'Grey')
-r7 = Room.create(name: 'Room7', color: 'Pink')
-r8 = Room.create(name: 'Room8', color: 'Grey')
-r9 = Room.create(name: 'Room9', color: 'Yellow')
