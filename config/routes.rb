a964971f9ae02b42acd4f5dabdb62474380cddca
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-11-27 11:31:08 +0000

    booking scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :bookings
   devise_for :users
   root to: "public#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
