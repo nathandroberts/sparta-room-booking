b6d6a8b194e3b0eecf8eaf76fc02f247da01548c
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-27 22:30:39 +0000

    view controller and route setup, date-week  variable can be changed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :bookings
   devise_for :users
   root to: "public#index"
+
+  get '/week', to: 'bookings#week'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
